8310f549717612068115bbf663b1ab3c10d194c5
Josh Benham <joshbenham@gmail.com>
2012-08-31 14:56:38 +1000

    Skeleton for my apps to install

---
+++ apps.rb
@@ -0,0 +1,63 @@
+dep 'Adium.app' do
+
+end
+
+dep 'Cyberduck.app' do
+
+end
+
+dep 'Firefox.app' do
+
+end
+
+dep 'Fluid.app' do
+
+end
+
+dep 'Google Chrome.app' do
+
+end
+
+dep 'Google Drive.app' do
+
+end
+
+dep 'ImageOptim.app' do
+
+end
+
+dep 'LimeChat.app' do
+
+end
+
+dep 'Sequel Pro.app' do
+
+end
+
+dep 'Skitch.app' do
+
+end
+
+dep 'Skype.app' do
+
+end
+
+dep 'Spectacle.app' do
+
+end
+
+dep 'Spotify.app' do
+
+end
+
+dep 'Sublime Text 2.app' do
+
+end
+
+dep 'VirtualBox.app' do
+
+end
+
+dep 'iTerm.app' do
+
+end
