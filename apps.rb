cf366526d523b3310c71b6d525a8111f76ea5cb1
Josh Benham <joshbenham@gmail.com>
2012-09-07 14:35:59 +1000

    Added 1Password, Adium, Cyber Duck and Firefox apps

--- apps.rb
+++ apps.rb
@@ -1,13 +1,17 @@
-dep 'Adium.app' do
+dep '1Password.app' do
+  source 'https://d13itkw33a7sus.cloudfront.net/dist/1P/mac/1Password-3.8.20.zip'
+end
 
+dep 'Adium.app' do
+  source 'http://sourceforge.net/projects/adium/files/Adium_1.5.3.dmg'
 end
 
 dep 'Cyberduck.app' do
-
+  souce 'http://cyberduck.ch/Cyberduck-4.2.1.zip'
 end
 
 dep 'Firefox.app' do
-
+  source 'http://www.mozilla.org/products/download.html?product=firefox-15.0.1&os=osx&lang=en-US'
 end
 
 dep 'Fluid.app' do
