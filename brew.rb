053c67536a932d0ea15419f8491c4cf509482d65
Josh Benham <joshbenham@gmail.com>
2012-09-10 13:57:51 +1000

    Added brew to the babushka deps

--- brew.rb
+++ brew.rb
@@ -2,14 +2,16 @@ dep 'ack.brew'
 dep 'ctags.brew'
 dep 'git.brew'
 dep 'jpeg.brew'
+dep 'mongo.brew'
 dep 'wget.brew'
 dep 'zsh.brew'
 
-dep ' brew items' do
+dep 'brew items' do
   requires 'ack.brew',
     'ctags.brew',
     'git.brew',
     'jpeg.brew',
+    'mongo.brew',
     'wget.brew',
     'zsh.brew'
 end
