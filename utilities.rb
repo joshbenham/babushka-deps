049f4cd95ded4178288a54fff7620fef28fccd30
Josh Benham <joshbenham@gmail.com>
2012-12-11 12:12:16 +1100

    Added the new utilities installer for linux

---
+++ utilities.rb
@@ -0,0 +1,26 @@
+dep 'ack-grep.managed' do
+  before {
+    sudo 'rm /usr/bin/ack'
+  }
+  after {
+    sudo 'ln -s /usr/bin/ack-grep /usr/bin/ack'
+    log 'Symlinked /usr/bin/ack-grep to /usr/bin/ack'
+  }
+end
+
+dep 'exuberant-ctags.managed' do
+  provides ['ctags']
+end
+dep 'git.managed'
+dep 'htop.managed'
+dep 'mercurial.managed'
+dep 'vim.managed'
+
+dep 'linux utilities installed' do
+  requires 'ack-grep.managed',
+    'exuberant-ctags.managed',
+    'git.managed',
+    'htop.managed',
+    'mercurial.managed',
+    'vim.managed'
+end
