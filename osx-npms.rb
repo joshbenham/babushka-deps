11cf40b4946558b640de57ec2f39e83f76a958df
Josh Benham <joshbenham@gmail.com>
2013-08-13 15:52:46 +1000

    Added grunt-init as an npm

--- osx-npms.rb
+++ osx-npms.rb
@@ -1,7 +1,9 @@
 dep 'grunt-cli.npm' do
   provides 'grunt'
 end
+dep 'grunt-init.npm'
 
 dep 'osx npms installed' do
-  requires 'grunt-cli.npm'
+  requires 'grunt-cli.npm',
+    'grunt-init.npm'
 end
